670a0256d394c7f62a9106166e892253d8278f22
alexnicolescu <nicolescualexandru12@gmail.com>
2020-03-07 18:31:05 +0200

    Refactoring and error messages

--- Test/client.c
+++ Test/client.c
@@ -12,25 +12,57 @@
 #define SERVERADDRESS "127.0.0.1" //Valoarea trebuie inlocuita cu adresa calculatorului pe care ruleaza serverul
 #define SERVERPORT 5678
 
+int sockfd,nread,errorCode=-1;
+char buf[1024],buf2[1024];
+struct sockaddr_in local_addr,remote_addr;
+void connectToServer()
+{
+	if((sockfd=socket(PF_INET,SOCK_STREAM,0))==-1)
+	{
+		perror("Unable to create a socket");
+		exit(errorCode--);
+
+	}
+	set_addr(&local_addr,NULL,INADDR_ANY,0);
+	if(bind(sockfd,(struct sockaddr*)&local_addr,sizeof(local_addr))==-1)
+	{
+		perror("Unable to bind a name to the socket");
+		exit(errorCode--);	
+	}
+	set_addr(&remote_addr,SERVERADDRESS,0,SERVERPORT);
+	if(connect(sockfd,(struct sockaddr*)&remote_addr,sizeof(remote_addr))==-1)
+	{
+		perror("Unable to connect to the socket");
+		exit(errorCode--);
+	}
+}
+
+
 int main(int argc,char*argv[]){
-	int sockfd;
-	char buf[1024],buf2[1024];
-	struct sockaddr_in local_addr,remote_addr;
-	int nread;
-	printf("Mesajul de transmis:\n");
+	
+	connectToServer();
+	printf("Mesesage:\n");
 	fgets(buf,1024,stdin);
 	buf[strlen(buf)-1]='\0';
-	sockfd=socket(PF_INET,SOCK_STREAM,0);
-	set_addr(&local_addr,NULL,INADDR_ANY,0);
-	bind(sockfd,(struct sockaddr*)&local_addr,sizeof(local_addr));
-	set_addr(&remote_addr,SERVERADDRESS,0,SERVERPORT);
-	connect(sockfd,(struct sockaddr*)&remote_addr,sizeof(remote_addr));
-	send(sockfd,buf,strlen(buf),0);
-        while(1){
-	nread=read(sockfd,(void*)buf2,1024);
-	buf2[nread]='\0';
-        if(nread>0)
-	printf("%s\n",buf2);}
-	close(sockfd);
-	exit(0);
-}
+	if(send(sockfd,buf,strlen(buf),0)==-1)
+	{
+		perror("Unable to send the message");
+		exit(errorCode--);
+	}
+	while(1){
+		nread=read(sockfd,(void*)buf2,1024);
+		if(nread<=0)
+		{
+			perror("Unable to read/No more information to read");
+			exit(errorCode--);
+		}
+		buf2[nread]='\0';
+		if(nread>0)
+			printf("%s\n",buf2);}
+		if(close(sockfd)==-1)
+		{
+			perror("Unable to close the socket");
+			exit(errorCode--);
+		}
+		exit(0);
+	}
--- Test/server.c
+++ Test/server.c
@@ -13,55 +13,110 @@
 #include "netio.h"
 
 #define SERVER_PORT 5678
+int errorCode=-1;
+int sockfd,connfd;
+char buf[1024];
+int fd,n,l,status;
+struct sockaddr_in local_addr,rmt_addr;
+int nread,p=0;
+char s[10];
+socklen_t rlen;
+pid_t pid;
 
-int main(void){
-	int sockfd,connfd;
-	char buf[1024];
-        int fd,n,l,status;
-	struct sockaddr_in local_addr,rmt_addr;
-	int nread,p=0;
-        char s[10];
-	socklen_t rlen;
-	sockfd=socket(PF_INET,SOCK_STREAM,0);
-	set_addr(&local_addr,NULL,INADDR_ANY,SERVER_PORT);
-	bind(sockfd,(struct sockaddr *)&local_addr,sizeof(local_addr));
-	listen(sockfd,5);
-	rlen=sizeof(rmt_addr);
-        
-        fd=open("a.txt",O_RDWR |O_APPEND |O_TRUNC);
-         if(fd==-1)
-          {printf("eroare deschidere fisier");
-          }
+void prepareToReceiveRequests()
+{
+  if((sockfd=socket(PF_INET,SOCK_STREAM,0))==-1){
+    perror("Unable to create a socket");
+    exit(errorCode--);
+  }
+  set_addr(&local_addr,NULL,INADDR_ANY,SERVER_PORT);
+  if(bind(sockfd,(struct sockaddr *)&local_addr,sizeof(local_addr))==-1)
+  {
+    perror("Unable to bind a name to the socket");
+    exit(errorCode--);  
+  }
+  if(listen(sockfd,5)==-1)
+  {
+    perror("Unable to listen for connections on this socket");
+    exit(errorCode--);
+  }
+  rlen=sizeof(rmt_addr);
+}
 
-        for(;;)
-        {
-         p++;
-         connfd=accept(sockfd,(struct sockaddr *)&rmt_addr,&rlen);
-         snprintf(s,sizeof(int),"%d",connfd);
-         write(fd,s,strlen(s));
-        
-         if(fork()==0)
-          {close(sockfd);
-	   nread=read(connfd,(void*)buf,1024);
-	   buf[nread]='\0';
-	   printf("%s\n",buf);
-	
-	  if(nread<0)
-	  {
-	   printf("Eroare la citirea de la retea\n");
-	  }
-        
-         lseek(fd,0,SEEK_SET);
-         while(l=(read(fd,s,sizeof(char)))>0)
-          {s[l]='\0';
-           n=atoi(s);
-	   write(n,(void*)buf,strlen(buf));
-          }
-	  exit(0);
-        }
-        }
-     close(fd);
-     close(connfd);
-     for(int i=0;i<=p;i++)
-      wait(&status);
+void openFile()
+{
+  fd=open("socketDescriptors.txt",O_RDWR |O_APPEND |O_TRUNC);
+  if(fd==-1)
+  {
+    perror("File not found");
+    exit(errorCode--);
+
+  }
+}
+
+int main(void)
+{
+  prepareToReceiveRequests();
+  openFile();
+  for(;;)
+  {
+   p++;
+   if((connfd=accept(sockfd,(struct sockaddr *)&rmt_addr,&rlen))==-1)
+   {
+    perror("Unable to accept a conection on this socket");
+    exit(errorCode--);
+  }
+  snprintf(s,sizeof(int),"%d",connfd);
+  if(write(fd,s,strlen(s))==-1)
+  {
+    perror("Unable to write the descriptor in the file");
+    exit(errorCode--);
+  }
+  pid=fork();
+  if(pid==-1)
+  {
+    perror("Unable to create a child process");
+    exit(errorCode--);
+  }
+  if(pid==0)
+  {
+    if(close(sockfd)==-1)
+    {
+      perror("Unable to close the socket");
+      exit(errorCode--);
+    }
+    nread=read(connfd,(void*)buf,1024);
+    if(nread<=0)
+    {
+      perror("Unable to receive information from client");
+      exit(errorCode--);
+    }
+    buf[nread]='\0';
+    printf("Message from a client:%s\n",buf);
+    lseek(fd,0,SEEK_SET);
+    while((l=read(fd,(void*)s,sizeof(char)))>0)
+    {
+      s[l]='\0';
+      n=atoi(s);
+      if(write(n,(void*)buf,strlen(buf))==-1)
+      {
+        perror("Unable to send the message to other clients");
+        exit(errorCode--);
+      }
+    }
+    exit(0);
+  }
+}
+if(close(fd)==-1)
+{
+  perror("Unable to close the file");
+  exit(errorCode--);
+}
+if(close(connfd)==-1)
+{
+  perror("Unable to close the socket");
+  exit(errorCode--);
+}
+for(int i=0;i<=p;i++)
+  wait(&status);
 }
