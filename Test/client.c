e1e0b7c22917c8b3f1b53d64ec43769b5c3862e4
alexnicolescu <nicolescualexandru12@gmail.com>
2020-03-13 10:18:19 +0200

    Reading Process And Writing Process In Client

--- Test/client.c
+++ Test/client.c
@@ -9,12 +9,14 @@
 #include<fcntl.h>
 #include<string.h>
 #include "netio.h"
+#include <sys/wait.h>
 #define SERVERADDRESS "127.0.0.1" //Valoarea trebuie inlocuita cu adresa calculatorului pe care ruleaza serverul
 #define SERVERPORT 5678
 
 int sockfd,nread,errorCode=-1;
 char buf[1024],buf2[1024];
 struct sockaddr_in local_addr,remote_addr;
+pid_t pid;
 void connectToServer()
 {
 	if((sockfd=socket(PF_INET,SOCK_STREAM,0))==-1)
@@ -37,17 +39,50 @@ void connectToServer()
 	}
 }
 
+void sigchldHandler(int signal)
+{
+	int status;
+	wait(&status);
+	if(!WIFEXITED(status))
+	{
+		perror("Child process ended with an error");
+		exit(errorCode--);
+	}
+}
+
+void handleSIGCHLD()
+{
+	struct sigaction actionForSIGCHLD;
+	actionForSIGCHLD.sa_flags=0;
+	sigemptyset(&(actionForSIGCHLD.sa_mask));
+	actionForSIGCHLD.sa_handler=sigchldHandler;
+	if(sigaction(SIGCHLD,&actionForSIGCHLD,NULL)<0)
+	{
+		perror("SIGCHLD error");
+		exit(errorCode--);
+	}
+}
+
 
 int main(int argc,char*argv[]){
 	
 	connectToServer();
-	printf("Mesesage:\n");
-	fgets(buf,1024,stdin);
-	buf[strlen(buf)-1]='\0';
-	if(send(sockfd,buf,strlen(buf),0)==-1)
+	pid=fork();
+	if(pid<0)
 	{
-		perror("Unable to send the message");
+		perror("Unable to create a child process");
 		exit(errorCode--);
+	}
+	if(pid==0)
+	{
+		printf("Mesesage:\n");
+		fgets(buf,1024,stdin);
+		buf[strlen(buf)-1]='\0';
+		if(send(sockfd,buf,strlen(buf),0)==-1)
+		{
+			perror("Unable to send the message");
+			exit(errorCode--);
+		}
 	}
 	while(1){
 		nread=read(sockfd,(void*)buf2,1024);
@@ -57,8 +92,10 @@ int main(int argc,char*argv[]){
 			exit(errorCode--);
 		}
 		buf2[nread]='\0';
-		if(nread>0)
-			printf("%s\n",buf2);}
+		if(nread>0){
+
+			printf("Mesesage from this/other clients:%s\n",buf2);}
+		}
 		if(close(sockfd)==-1)
 		{
 			perror("Unable to close the socket");
